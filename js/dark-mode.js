21985b20648ec8cfa7e768bfc0ef5a3fd291fb53
Vitor Rey <vitormoises.rey@gmail.com>
2020-09-23 22:40:47 -0300

    revisão

--- js/dark-mode.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var element = document.body;
-  var card = document.getElementsByClassName("card-body")
-  element.classList.toggle("dark-mode");
-  for(let i = 0;i< card.length; i++){
-    card[i].classList.toggle("black-card");
-  }
-  
-}
